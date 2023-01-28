91cbaabb285e02544d18edc7992e1f33ab1a200b
Garrick He <garrickhe@gmail.com>
2023-01-28 09:10:34 -0800

    Add Listening Port
    Signed-off-by: Garrick He <garrickhe@gmail.com>

--- app/main.py
+++ app/main.py
@@ -1,16 +1,12 @@
-# Uncomment this to pass the first stage
-# import socket
+import socket
 
+HOST="localhost"
+PORT=6379
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
-    # server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    # server_socket.accept() # wait for client
-
+    server_socket = socket.create_server((HOST, PORT), reuse_port=True)
+    server_socket.accept() # wait for client
+    print("{} listning on port: {}".format(HOST, PORT))
 
 if __name__ == "__main__":
     main()
