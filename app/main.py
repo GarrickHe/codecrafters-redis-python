c3fdfd7cf8c40170a831a6681c04cdedaa53a628
Garrick He <garrickhe@gmail.com>
2023-01-28 13:57:51 -0800

    Process simple Redis PING cmd
    Signed-off-by: Garrick He <garrickhe@gmail.com>

--- app/main.py
+++ app/main.py
@@ -2,11 +2,16 @@ import socket
 
 HOST="localhost"
 PORT=6379
+BUF_SIZE_IN_BYTES=1024
+
+def simpleString(s):
+    return str.encode('+' + s + '\r\n')
 
 def main():
-    server_socket = socket.create_server((HOST, PORT), reuse_port=True)
-    server_socket.accept() # wait for client
     print("{} listning on port: {}".format(HOST, PORT))
-
+    server_socket = socket.create_server((HOST, PORT), reuse_port=True)
+    conn, addr = server_socket.accept() # wait for client
+    conn.sendall(simpleString('PONG'))
+    
 if __name__ == "__main__":
     main()
