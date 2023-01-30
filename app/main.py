11ad40aaa88a68757a391331d815f5237414bebb
Garrick He <garrickhe@gmail.com>
2023-01-29 18:27:24 -0800

    Add while-loop for multiple PINGs
    * Add while-loop to support multiple PING commands
    * Conform to PEP8 standards
    
    Signed-off-by: Garrick He <garrickhe@gmail.com>

--- app/main.py
+++ app/main.py
@@ -1,17 +1,22 @@
 import socket
 
-HOST="localhost"
-PORT=6379
-BUF_SIZE_IN_BYTES=1024
+HOST = 'localhost'
+PORT = 6379
+BUF_SIZE_IN_BYTES = 1024
+
 
 def simpleString(s):
     return str.encode('+' + s + '\r\n')
 
+
 def main():
     print("{} listning on port: {}".format(HOST, PORT))
     server_socket = socket.create_server((HOST, PORT), reuse_port=True)
-    conn, addr = server_socket.accept() # wait for client
-    conn.sendall(simpleString('PONG'))
-    
+    while True:
+        conn, ipAddr = server_socket.accept()  # wait for client
+        print('Connection from: {}'.format(ipAddr))
+        conn.sendall(simpleString('PONG'))
+
+
 if __name__ == "__main__":
     main()
