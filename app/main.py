6d6b57a7d324a7f36cc00feb83719c8bb75ac734
Garrick He <garrickhe@gmail.com>
2023-01-29 18:45:53 -0800

    Update loop logic
    * Update logic in loop to close connection
    * Disable debug
    
    Signed-off-by: Garrick He <garrickhe@gmail.com>

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 HOST = 'localhost'
 PORT = 6379
@@ -8,15 +9,21 @@ BUF_SIZE_IN_BYTES = 1024
 def simpleString(s):
     return str.encode('+' + s + '\r\n')
 
+def handler(conn):
+    conn.sendall(simpleString('PONG'))
+    conn.close()
 
 def main():
     print("{} listning on port: {}".format(HOST, PORT))
     server_socket = socket.create_server((HOST, PORT), reuse_port=True)
+    conn, ipAddr = server_socket.accept()  # wait for client
+    print('Connection from: {}'.format(ipAddr))
     while True:
-        conn, ipAddr = server_socket.accept()  # wait for client
-        print('Connection from: {}'.format(ipAddr))
+        data = conn.recv(BUF_SIZE_IN_BYTES)
+        if len(data) == 0:
+            break
         conn.sendall(simpleString('PONG'))
-
+    conn.close()
 
 if __name__ == "__main__":
     main()
