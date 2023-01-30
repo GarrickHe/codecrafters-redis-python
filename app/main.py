8ef7f3db7e38da993365ed7b368fa42be877e5ec
Garrick He <garrickhe@gmail.com>
2023-01-29 20:36:57 -0800

    Use asyncio for multiconnection
    Signed-off-by: Garrick He <garrickhe@gmail.com>

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import socket
-import threading
+import asyncio
 
 HOST = 'localhost'
 PORT = 6379
@@ -9,21 +9,22 @@ BUF_SIZE_IN_BYTES = 1024
 def simpleString(s):
     return str.encode('+' + s + '\r\n')
 
-def handler(conn):
-    conn.sendall(simpleString('PONG'))
-    conn.close()
 
-def main():
-    print("{} listning on port: {}".format(HOST, PORT))
-    server_socket = socket.create_server((HOST, PORT), reuse_port=True)
-    conn, ipAddr = server_socket.accept()  # wait for client
-    print('Connection from: {}'.format(ipAddr))
+async def handler(reader, writer):
+    print(f'Connection from: {writer.get_extra_info("peername")}')
     while True:
-        data = conn.recv(BUF_SIZE_IN_BYTES)
+        data = (await reader.read(BUF_SIZE_IN_BYTES))
         if len(data) == 0:
             break
-        conn.sendall(simpleString('PONG'))
-    conn.close()
+        writer.write(simpleString('PONG'))
+    writer.close()
+
+
+async def main():
+    print("{} listning on port: {}".format(HOST, PORT))
+    server = await asyncio.start_server(handler, HOST, PORT)
+    async with server:
+        await server.serve_forever()
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
